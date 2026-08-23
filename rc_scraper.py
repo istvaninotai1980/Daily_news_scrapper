fbca4f879bb7331d1cc3e170b9e6ec93b4006b24
istvaninotai1980 <istvan.inotai1980@gmail.com>
2026-08-23 22:27:55 +0200

    Update rc_scraper.py

--- rc_scraper.py
+++ rc_scraper.py
@@ -7,7 +7,7 @@ from email.mime.multipart import MIMEMultipart
 
 # --- CONFIG ---
 URL = "https://www.rc-network.de/forums/biete-flugmodelle.132/"
-KEYWORDS = ["ccm toy", "toy", "swift"]
+KEYWORDS = ["ccm toy", "toy", "F5D", "hotliner", "mahmoudi"]
 LAST_SEEN_FILE = "last_seen_id.txt"
 
 def get_forum_threads():
